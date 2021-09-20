d4addc0742c948b3e082397c3448949cbcff8db0
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-09-21 00:18:58 +0900

    add the memo create func

--- server/src/main.go
+++ server/src/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 type Post struct {
 	Content string `json:"content"`
@@ -19,4 +23,28 @@ type Server struct {
 func (s *Server) RegisterRoutes() {
 	http.HandleFunc("/memo/create", s.CreateMemo)
 	http.HandleFunc("/memo/list", s.ListMemos)
+}
+
+func (s *Server) CreateMemo(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Not found", 404)
+		// return will stop the func
+		return
+	}
+
+	var post Post
+	// := means new create variable
+	// NewDecoder will read your json body string, Decode will turn the json body to post type
+	// &post is a pointer to the post value
+	// if successed, the post will get the value, if valued, it gave you the err
+	err := json.NewDecoder(r.Body).Decode(&post)
+
+	if err != nil {
+		http.Error(w, "Invalid memo: "+err.Error(), 400)
+		return
+	}
+
+	s.db.Posts = append(s.db.Posts, post)
+
+	fmt.Fprintf(w, "Memo created")
 }
